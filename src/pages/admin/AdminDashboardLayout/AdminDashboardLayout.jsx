c98289b06f6b517f2f51146d97b2dffaafbd146c
Kesara <kulathungakgr@gmail.com>
2025-04-28 16:29:05 +0530

    admin basic done

--- src/pages/admin/AdminDashboardLayout/AdminDashboardLayout.jsx
+++ src/pages/admin/AdminDashboardLayout/AdminDashboardLayout.jsx
@@ -49,23 +49,23 @@ export default function AdminLayout({ mobileNumber = '123-456-7890' }) {
       {/* Sidebar */}
       <aside className="admin-sidebar">
         <nav className="admin-sidebar__nav">
-          <NavLink to="view-admins"    className="admin-sidebar__link"> 
+          <NavLink to="/admin/dashboard/view-admins"    className="admin-sidebar__link"> 
             <img src={viewAdminsIcon} className="admin-sidebar__icon" alt="" />
             View Admins
           </NavLink>
-          <NavLink to="update-slots"   className="admin-sidebar__link">
+          <NavLink to="/admin/dashboard/update-slots"   className="admin-sidebar__link">
             <img src={slotsIcon}      className="admin-sidebar__icon" alt="" />
             Update Time Slots
           </NavLink>
-          <NavLink to="view-reports"   className="admin-sidebar__link">
+          <NavLink to="/admin/dashboard/view-reports"   className="admin-sidebar__link">
             <img src={reportsIcon}    className="admin-sidebar__icon" alt="" />
             View Reports
           </NavLink>
-          <NavLink to="delete-profile" className="admin-sidebar__link">
+          <NavLink to="/admin/dashboard/delete-profile" className="admin-sidebar__link">
             <img src={deleteIcon}     className="admin-sidebar__icon" alt="" />
             Delete Profile
           </NavLink>
-          <NavLink to="logout"         className="admin-sidebar__link">
+          <NavLink to="/admin/dashboard/logout"         className="admin-sidebar__link">
             <img src={logoutIcon}     className="admin-sidebar__icon" alt="" />
             Log Out
           </NavLink>
